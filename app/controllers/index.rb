1c694c9a6ccc73d66cccd96e0a191cea8c0c4c15
Matthew Villwock <mtvillwock@gmail.com>
2015-03-02 15:24:06 -0800

    add location to user migration, make post route to add user to database

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,11 @@
 get '/' do
 
-	
+
   erb :index
 end
 
+post '/' do
+  @user = User.new(name: params[:name], location: params[:location])
+  content_type :json
+  {name: @user.name, location: @user.location}.to_json
+end
