1d8f78ec03fdda06fa5eeb7f03a6011f2ab11aba
Matthew Villwock <mtvillwock@gmail.com>
2015-03-02 12:17:38 -0800

    add API key and scripts for google maps

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
-	
+// $(document).ready(function() {
 
 
-});
+
+// });
+
+
+function initialize() {
+  var mapOptions = {
+    center: { lat: -34.397, lng: 150.644},
+    zoom: 8
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
