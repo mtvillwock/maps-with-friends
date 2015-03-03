9b39269067ae76a1fa7163d039cd5c6a67b83418
Matthew Villwock <mtvillwock@gmail.com>
2015-03-02 16:48:30 -0800

    add p statements to check if server was receiving data

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,11 @@ end
 
 post '/' do
   @user = User.new(name: params[:name], location: params[:location])
+  p @user
+  p "executed user.new"
   if @user.save
+    p @user
+    p "user saved"
     content_type :json
     {name: @user.name, location: @user.location}.to_json
   else
