af32d2513dbbdfa89b5390083ac999472ce8b57d
Matthew Villwock <mtvillwock@gmail.com>
2015-03-06 23:01:43 -0800

    break register and login into own pages, login form isn't working... can't find userfor some reason, despite new users being saved to database after registering

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,8 +1,14 @@
+# =========================
+# Index
+# =========================
 get '/' do
 
   erb :index
 end
 
+# =========================
+# Make a Friend Marker
+# =========================
 post '/' do
   @friend = Friend.new(name: params[:name], location: params[:location])
   content_type :json
@@ -16,9 +22,12 @@ post '/' do
 end
 
 
-# AJAX call to populate map
+# =========================
+# Populate Map from User's friends
+# =========================
 get '/locations' do
-  @friends = Friend.all
+  @user = current_user
+  @friends = User.friends
   friends_and_locations = []
   content_type :json
   @friends.each do |friend, location|
@@ -28,29 +37,55 @@ get '/locations' do
   friends_and_locations.to_json
 end
 
+# =========================
+# Register
+# =========================
+get '/register' do
+
+  erb :register
+end
+
 post '/register' do
   content_type :json
   # AJAX switch login to sign-in
   user = User.new(params[:user])
     if user.save
       {welcome: "Greetings, #{user.username}"}.to_json
+      redirect '/login'
     else
       {error: "Something went wrong."}.to_json
+      erb :register
     end
 end
 
+# =========================
+# Login
+# =========================
+get '/login' do
+
+  erb :login
+end
+
 post '/login' do
-  content_type :json
   # AJAX sign-in
+  p params
   @user = User.find_by(email: params[:email])
+  p @user
+  p @user.password
   if @user && @user.password == params[:password]
     session[:user_id] = @user.id
+    p session[:user_id]
+    redirect '/'
   else
-    {error: "Incorrect password"}.to_json
+    erb :login
   end
 end
 
+# =========================
+# Logout
+# =========================
 delete '/logout' do
   # AJAX sign out (or just send home?)
   session[:user_id] = nil
+  redirect '/'
 end
--- db/migrate/20150306161540_create_users.rb
+++ db/migrate/20150306161540_create_users.rb
@@ -1,5 +1,3 @@
-require 'bcrypt'
-
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
@@ -7,14 +5,5 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_hash
     end
-  end
-
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
   end
 end
