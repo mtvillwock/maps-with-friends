0576a57c2e4cb3a08b0eddcd089a896e0d896307
Matthew Villwock <mtvillwock@gmail.com>
2015-03-06 17:16:32 -0800

    add current_user helper method, style form

---
+++ app/helpers/sessions.rb
@@ -0,0 +1,7 @@
+helpers do
+
+  def current_user
+    @user = User.find(session[:user_id]) unless session[:user_id] == nil
+  end
+
+end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,4 +2,5 @@ class Friend < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :name, :location
+  validates_uniqueness_of :name
 end
