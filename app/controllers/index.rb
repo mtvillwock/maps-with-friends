5f10024683c03cd6d987002386532007e038b5c7
Matthew Villwock <mtvillwock@gmail.com>
2015-04-01 11:35:52 -0700

    considering punting this branch because I'll have to revise the schema again once I add OAuth anyway, so I'd rather get that going first, add console.log for data, remove/make comments in index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,17 +11,21 @@ get '/' do
 end
 
 # =========================
-# Make a Friend and Friendship
+# Make a User / Friendship
 # =========================
 post '/' do
   @user = User.find_by(id: session[:user_id])
-  @friend = Friend.find_or_initialize_by(name: params[:name])
+  p "current user is: "
+  p @user
+  @friend = User.find_or_initialize_by(name: params[:name])
+  p "new friend is: "
   @friend.update_attributes(location: params[:location])
+  p @friend
   if @friend.location != nil
     content_type :json
     if @friend.save
       @friendship = Friendship.create(friend_id: @friend.id, user_id: current_user.id)
-      # Try @friendship = @user.friendships.create(params[:friendship])
+      p @friendship
       {name: @friend.name, location: @friend.location, id: @friend.id}.to_json
     else
       {error: "Friend did not save"}.to_json
@@ -33,15 +37,15 @@ end
 # Delete a Friend and Friendship
 # =========================
 
-delete '/friends/:id/delete' do
-  @friend = Friend.find(params[:id])
-  content_type :json
-  if @friend.delete
-    { id: params[:id] }.to_json
-  else
-    { error: "Friend not destroyed"}.to_json
-  end
-end
+# delete '/friends/:id/delete' do
+#   @friend = Friend.find(params[:id])
+#   content_type :json
+#   if @friend.delete
+#     { id: params[:id] }.to_json
+#   else
+#     { error: "Friend not destroyed"}.to_json
+#   end
+# end
 
 # =========================
 # Populate Map from User's friends
@@ -49,17 +53,27 @@ end
 get '/locations' do
   @user = User.find_by(id: session[:user_id])
   @friendships = @user.friendships
-  @friendships.each do |friendship|
-    @friends << User.find_by(id: friendship.friend_id)
+  p "friendships: "
+  p @friendships
+  @friends = []
+  if !@friendships.empty?
+    # @friendships.each do |friendship|
+      @friends << User.find_by(id: friendship.friend_id).name
+  #   end
+  #   p @friends.each { |f| f.name }
+    content_type :json
+  #   # @friends.each do |friend|
+  #   #   friend = {name: friend.name, location: friend.location}
+  #   #   friends_and_locations << friend
+  #   # end
+  #   # friends_and_locations.to_json
+  #   if @friends
+      @friends.to_json
+    # end
+  else
+    content_type :json
+    {error: "No friendships found"}.to_json
   end
-  p @friends.each { |f| f.name }
-  content_type :json
-  # @friends.each do |friend|
-  #   friend = {name: friend.name, location: friend.location}
-  #   friends_and_locations << friend
-  # end
-  # friends_and_locations.to_json
-  @friends.to_json
 end
 
 # =========================
@@ -72,11 +86,11 @@ end
 
 post '/register' do
   user = User.new(params[:user])
-    if user.save
-      redirect '/login'
-    else
-      erb :register
-    end
+  if user.save
+    redirect '/login'
+  else
+    erb :register
+  end
 end
 
 # =========================
