0cc5882ba03bfd5b6639604680a1dc73ffbf7247
Matthew Villwock <mtvillwock@gmail.com>
2015-04-03 17:12:44 -0700

    add skeletons of RSPEC controller tests

---
+++ spec/controller/login_spec.rb
@@ -0,0 +1,12 @@
+describe 'Login Controller' do
+  context "GET /register" do
+  end
+  context "POST /register" do
+  end
+  context "GET /login" do
+  end
+  context "POST /login" do
+  end
+  context "GET /logout" do
+  end
+end
