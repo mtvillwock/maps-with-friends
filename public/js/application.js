e3aec9556c5bd736a2f3fb16ebe9b9bef0216e67
Matthew Villwock <mtvillwock@gmail.com>
2015-08-03 17:26:56 -0700

    revert to working state; tweak ERB/JS so adding friends displays same HTML, still need to figure out how to remove markers from map when deleting friends - currently you must refresh the page to show that

--- public/js/application.js
+++ public/js/application.js
@@ -43,7 +43,7 @@ function addNewMarker(e){
     // navigator.geolocation.getCurrentPosition(codeAddress);
     codeAddress(data);
     // Makes the new marker based on address in form submission
-    $("#friend-list").append("<p>" + "<span class='friend-name'>" + data.name + "</span>" + " in " + "<span class='friend-location'>" + data.location + "</span>" + "</p>");
+    $("#friend-list").append("<li><p>" + "<span class='friend-name'>" + data.name + "</span>" + " in " + "<span class='friend-location'>" + data.location + "</span>" + "</p>" + "<button><a class='delete' href='/friend/'" + data.id + "/delete>Delete</a></button></li>");
     console.log("form should clear");
     clearFriendForm();
   });
@@ -202,7 +202,8 @@ function populateLocations() {
 
       addMarkerFromDatabase(data[i]);
 
-      $("#friend-list").append("<p>" + "<span class='friend-name'>" + data[i].name + "</span>" + " in " + "<span class='friend-location'>" + data[i].location + "</span>" + "</p>");
+      // $("#friend-list").append("</li><p>" + "<span class='friend-name'>" + data[i].name + "</span>" + " in " + "<span class='friend-location'>" + data[i].location + "</span>" + "</p>" + "<button><a class='delete' href='/friend/'" + data.id + "/delete>Delete</a></button></li>"
+        // );
 
     };
   });
